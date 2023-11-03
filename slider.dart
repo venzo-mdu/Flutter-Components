0650ed9a1dd8acfe5cc1eed81ba6f51b1a576bcd
santhiya107 <104203831+santhiya107@users.noreply.github.com>
2023-11-03 12:33:19 +0530

    Slider Updated

--- slider.dart
+++ slider.dart
@@ -5,21 +5,31 @@ class CustomSlider extends StatefulWidget {
   final double min;
   final double max;
   final ValueChanged<double> onChanged;
+  final ValueChanged<double>? onSliderChanged;
   final Color? activeColor;
   final Color? inactiveColor;
   final String? label;
+  final int? divisions;
+  final String sliderLabel;
   final Color? secondaryActiveColor;
+  final TextEditingController sliderController;
+  final InputDecoration? decoration;
 
   const CustomSlider({
     super.key,
     this.activeColor,
     this.max = 1.0,
     required this.onChanged,
+    this.onSliderChanged,
     required this.value,
     this.min = 0.0,
     this.inactiveColor,
     this.label,
     this.secondaryActiveColor,
+    this.divisions,
+    required this.sliderLabel,
+    required this.sliderController,
+    this.decoration = const InputDecoration()
   });
 
   @override
@@ -29,15 +39,42 @@ class CustomSlider extends StatefulWidget {
 class _CustomSliderState extends State<CustomSlider> {
 
   @override
+  void initState() {
+    super.initState();
+    widget.sliderController.text = widget.value.round().toString();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Slider(
-      secondaryActiveColor:widget.secondaryActiveColor,
-      value: widget.value,
-      max: widget.max,
-      onChanged: widget.onChanged,
-      label: widget.label,
-      inactiveColor :widget.inactiveColor,
-      activeColor: widget.activeColor,
+    return  Column(
+      children: [
+        Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+               Text(
+                widget.sliderLabel,
+              ),
+              IntrinsicWidth(
+                  child: TextField(
+                controller: widget.sliderController,
+                decoration: widget.decoration,
+              ))
+            ],
+          ),
+        ),
+        Slider(
+          secondaryActiveColor:widget.secondaryActiveColor,
+          value: widget.value,
+          max: widget.max,
+          onChanged: widget.onChanged,
+          label: widget.label,
+          inactiveColor :widget.inactiveColor,
+          activeColor: widget.activeColor,
+          divisions: widget.divisions,
+        )
+      ],
     );
   }
 }
