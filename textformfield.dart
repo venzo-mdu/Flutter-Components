44e75a22cbca23aecd386ce22ffc6b5978865d7e
sivasakthi26 <104203753+sivasakthi26@users.noreply.github.com>
2023-11-03 12:31:32 +0530

    textformfield.dart

--- textformfield.dart
+++ textformfield.dart
@@ -28,23 +28,71 @@ Widget textMedium(
   );
 }
 
-Widget textformfieldMedium() {
-  return TextFormField(
-    cursorColor: Colors.white,
-    decoration: const InputDecoration(
-
-        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)
-        ),
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(6),
+Widget textformfieldMedium(
+    {required String hint,
+      required TextEditingController controller,
+      required validate,
+      autofillhints,
+      hidden}) {
+  return GestureDetector(
+      child: Container(
+          decoration: BoxDecoration(
+            color:Colors.white,
+            borderRadius: BorderRadius.circular(3),
+            border: Border.all(
+              color:Colors.white,
             ),
+          ),
+          child:
+          TextFormField(
+            autofocus: false,
+            controller: controller,
+            decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(6),
+                  ),
+                  borderSide: BorderSide(color: Colors.white, width: 3,)),
+              hintText: hint,
+              hintStyle: TextStyle(
+                  fontSize: 16,
+                  fontFamily: "Gilroy Regular",
+                  color: Colors.white
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 15,
+              ),
+            ),
+            validator: validate,
+          )
 
-            borderSide: BorderSide(color: Colors.white, width: 3,))),
-  );
+      )
+
+  )
+  ;
 }
+Widget StyledContainer(
+    final double width,
+    final double boderRadius,
+    final Widget child,
+    final Color color,
+    ){
+  return Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(boderRadius),
+      border: Border.all(
+        color:Color(0xffFFBE78),
+      ),
+      color: color,
+
+    ),
+    child: child,
+    width: width,
 
 
+  );
 
+}
 
 
 
